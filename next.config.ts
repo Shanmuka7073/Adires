0eb08de3fca4de0f65b0e0110507517a10d22c88
shanmuka srinivas <shanmuka7073@gmail.com>
2025-11-13 16:48:43 +0000

    Check cache is it making crash

--- next.config.ts
+++ next.config.ts
@@ -50,19 +50,6 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '4.5mb',
     },
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default pwaConfig(nextConfig);
