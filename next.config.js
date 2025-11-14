10b35728dcf68a69da2e66bbc83ec450063ee173
shanmuka srinivas <shanmuka7073@gmail.com>
2025-11-14 17:03:41 +0000

    You're facing a specific configuration error in your `next.config.js` fi

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
