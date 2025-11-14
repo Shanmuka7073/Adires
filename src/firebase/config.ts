5ecd4631e5a24195c2460e7d8ea80b03a738a7f8
shanmuka srinivas <shanmuka7073@gmail.com>
2025-11-14 19:36:28 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-9070259337-c267a",
+  "appId": "1:215572467483:web:88e4f0f118aa0d57b1cdbe",
+  "apiKey": "AIzaSyA6Qnsk2vz3GfABFJMM53FhNCAf74SBarE",
+  "authDomain": "studio-9070259337-c267a.firebaseapp.com",
+  "storageBucket": "studio-9070259337-c267a.appspot.com",
+  "measurementId": "",
+  "messagingSenderId": "215572467483"
 };
