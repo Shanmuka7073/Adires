73d88281921502576c960de26671190f82da2e9b
shanmuka srinivas <shanmuka7073@gmail.com>
2025-11-16 08:45:48 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,12 @@
-/**
- * @fileoverview This file exports the client-side Firebase configuration object.
- * It is used by the client-side initialization logic in `/src/firebase/index.ts`.
- */
+
+// This file is intended for client-side Firebase configuration.
+// It securely loads credentials from environment variables.
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: 'monospace-10.firebaseapp.com',
-  projectId: 'monospace-10',
-  storageBucket: 'monospace-10.appspot.com',
-  messagingSenderId: '1098358237307',
-  appId: '1:1098358237307:web:e69d3e875796f23a63d958',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
