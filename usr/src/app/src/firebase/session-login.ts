3117f150335646b91a5a765d47873d5e9517372f
shanmuka srinivas <shanmuka7073@gmail.com>
2025-11-17 20:58:18 +0000

    Next.js (14.2.33) is outdated (learn more) ./src/firebase/session-login.

---
+++ usr/src/app/src/firebase/session-login.ts
@@ -0,0 +1,8 @@
+'use server';
+
+// This file is temporarily disabled as all Firebase Admin SDK functionality has been removed
+// for client-side development focus. It can be restored when server-side features are needed.
+
+export async function sessionLogin(idToken: string) {
+  console.log('Session login is disabled for client-side development.');
+}
