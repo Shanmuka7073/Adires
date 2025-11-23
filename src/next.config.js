7fce5a9b58329c21686d4f7b02182f9a685b24cb
shanmuka srinivas <shanmuka7073@gmail.com>
2025-11-23 15:19:43 +0000

    NotAllowedError: The 'publickey-credentials-create' feature is not enabl

---
+++ src/next.config.js
@@ -0,0 +1,68 @@
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  manifest: false, // Tell next-pwa to not generate a manifest link
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'publickey-credentials-create=*',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      }
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4.5mb',
+    },
+    esmExternals: 'loose',
+  },
+  transpilePackages: ['firebase', '@firebase/auth', '@firebase/firestore'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
