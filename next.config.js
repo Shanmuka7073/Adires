f338ccca3e7ec18a52b27c1e8f85a081c000b0e2
shanmuka srinivas <shanmuka7073@gmail.com>
2025-12-03 18:32:50 +0000

    add pwa manifest

---
+++ next.config.js
@@ -0,0 +1,67 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value:
+              'publickey-credentials-create=(self), publickey-credentials-get=(self)',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      }
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4.5mb',
+    },
+    esmExternals: 'loose',
+  },
+  transpilePackages: ['firebase', '@firebase/auth', '@firebase/firestore'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
