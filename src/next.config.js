d01dcda27b16276c2e5506e2238b731135061e88
shanmuka srinivas <shanmuka7073@gmail.com>
2025-12-04 05:31:40 +0000

    Install app is not showing means something pwa capabilities is missing

--- src/next.config.js
+++ src/next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: false, // Temporarily enable for development testing
   manifest: false, // Tell next-pwa to not generate a manifest link
 });
 
