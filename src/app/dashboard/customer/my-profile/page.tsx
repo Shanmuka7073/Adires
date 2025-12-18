f5af42dee68525745d968ed4ea292f8f320adfc0
shanmuka srinivas <shanmuka7073@gmail.com>
2025-12-18 08:47:10 +0000

    1 of 1 error Next.js (14.2.35) is outdated (learn more)
    Unhandled Runti

--- src/app/dashboard/customer/my-profile/page.tsx
+++ src/app/dashboard/customer/my-profile/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useFirebase, useDoc, useMemoFirebase, errorEmitter, FirestorePermissionError } from '@/firebase';
-import { useForm } from 'react-hook-form';
+import { useForm, useFormContext } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Button } from '@/components/ui/button';
